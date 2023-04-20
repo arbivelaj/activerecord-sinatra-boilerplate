53973ae340170eefad2451df98c3b8a5d5fbc540
Arbi Velaj <arbi.velaj@gmail.com>
2023-04-20 19:08:13 +0100

    part one completed

--- app.rb
+++ app.rb
@@ -11,3 +11,21 @@ get "/contacts" do
   @batch = "1176"
   erb :contacts
 end
+
+get "/restaurants" do
+  # get all the restaurants and make them available in the view
+  @restaurants = Restaurant.all
+  # render the view
+  erb :restaurants
+end
+
+get "/restaurants/:id" do
+  # params[:id] use this value to find the corresponding restaurant
+end
+
+
+# /restarants/1
+# /restarants/2
+# /restarants/3
+# /restarants/4
+# ...
